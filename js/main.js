d990ca0df431564d4b070460932099687815ef82
David Gailey <dave.g.github@gmail.com>
2018-08-10 10:49:01 -0400

    test: 'DOMContentLoaded' appears to fire 3 times because there are 3 calls to the restaurants api

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ var markers = []
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
 document.addEventListener('DOMContentLoaded', (event) => {
+  console.log('DOMContentLoaded');
   initMap(); // added 
   fetchNeighborhoods();
   fetchCuisines();
