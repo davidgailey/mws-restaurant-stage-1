c42047e9431d97e02257d4242285156fa6e2da53
David Gailey <dave.g.github@gmail.com>
2018-09-24 02:08:26 -0400

    feature: review caching to idb

--- src/sw.js
+++ src/sw.js
@@ -1,14 +1,26 @@
 import idb from 'idb';
 
-const idbPromise = idb.open('mws-restaurant', 1, upgradeDB => {
+const idbPromise = idb.open('mws-restaurant', 2, upgradeDB => {
 	switch (upgradeDB.oldVersion) {
 		case 0:
 			upgradeDB.createObjectStore('restaurants', {
 				keyPath: 'id'
 			});
-			
+		case 1:
+			{
+				const idbReviewsStore = upgradeDB.createObjectStore("reviews", {
+					keyPath: "id"
+				});
+				idbReviewsStore.createIndex("byrestaurantid", "byrestaurantid");
+
+				upgradeDB.createObjectStore("pendingReviewPosts", {
+					keyPath: "id",
+					autoIncrement: true // this should create sequencial keys that can later be cursored over in order
+				});
+			}
 	}
 });
+
 
 const staticAssetsCacheName = 'mws-restaurant-static-v2';
 const assets = [
@@ -49,10 +61,15 @@ self.addEventListener('fetch', fetchEvent => {
 
 	// if it is an api call, handle it with indexedDB
 	if (isAPI) {
-		const id = requestURL.pathname.split( '/' )[1];
-		
-		if(typeof id !== 'undefined') handleAPIRequest(fetchEvent, id);
-	
+		const id = requestURL.pathname.split('/')[1];
+		const isReview = requestURL.pathname.match('reviews') ? true : false;
+
+		if (isReview) {
+			handleReviewRequest(fetchEvent);
+		} else if (typeof id !== 'undefined') {
+			handleAPIRequest(fetchEvent, id);
+		}
+
 	} else {
 		// if it is a normal request, handle with cache
 		fetchEvent.respondWith(
@@ -64,51 +81,105 @@ self.addEventListener('fetch', fetchEvent => {
 });
 
 const handleAPIRequest = (fetchEvent, id) => {
-	
+
 	fetchEvent.respondWith(
 
 		idbPromise // from idb.open on line 3ish
 
-			// check indexedDB to see if json from the API is already stored... 
-			.then(idb => {
-				return idb.transaction('restaurants').objectStore('restaurants').get(id);
-			})
-			.then(restaurantInfo => {
-				if (restaurantInfo && restaurantInfo.data){
-					// if yes then return restaurantInfo
-					// means earlier idb transaction succeeded
-					// return new Response(restaurantInfo.data);
-					console.log('restaurant info returned from indexedDB');
-					return restaurantInfo.data
-				}else{
-					// or if json is not stored in the idb, fetch it
-					// means earlier idb transaction failed
-					console.log('restaurant info will return from fetch');
-					return(
-						fetch(fetchEvent.request)
-						.then(fetchResponse => fetchResponse.json())
-						.then(json => {
-							return idbPromise.then(db => {
-								// new transaction
-								let tx = db.transaction('restaurants', 'readwrite');
-								// select store
-								let store = tx.objectStore('restaurants');
-								// write json to the idb store
-								store.put({ id: id, data: json });
-								// return the json to next 'then' statement
-								return json;
+		// check indexedDB to see if json from the API is already stored... 
+		.then(idb => {
+			return idb.transaction('restaurants').objectStore('restaurants').get(id);
+		})
+		.then(restaurantInfo => {
+			if (restaurantInfo && restaurantInfo.data) {
+				// if yes then return restaurantInfo
+				// means earlier idb transaction succeeded
+				// return new Response(restaurantInfo.data);
+				console.log('restaurant info returned from indexedDB');
+				return restaurantInfo.data
+			} else {
+				// or if json is not stored in the idb, fetch it
+				// means earlier idb transaction failed
+				console.log('restaurant info will return from fetch');
+				return (
+					fetch(fetchEvent.request)
+					.then(fetchResponse => fetchResponse.json())
+					.then(json => {
+						return idbPromise.then(db => {
+							// new transaction
+							let tx = db.transaction('restaurants', 'readwrite');
+							// select store
+							let store = tx.objectStore('restaurants');
+							// write json to the idb store
+							store.put({
+								id: id,
+								data: json
+							});
+							// return the json to next 'then' statement
+							return json;
+						});
+					})
+				)
+			}
+
+		}).then(response => {
+			return new Response(JSON.stringify(response));
+		})
+
+		.catch(error => {
+			console.log('error in handleAPIRequest: ', error);
+			return new Response('Something went terribly wrong!', {
+				status: 500
+			});
+		})
+	);
+}
+
+const handleReviewRequest = (fetchEvent) => {
+	fetchEvent.respondWith(
+		idbPromise
+		.then(idb => {
+			// first attempt to get review from idb
+			return idb
+				.transaction("reviews").objectStore("reviews")
+				.index("byrestaurantid")
+				.getAll(id);
+		}).then(data => {
+			// return the retrieved data
+			// if it's not there, try a fetch
+			return (data.length && data) || fetch(fetchEvent.request)
+				// convert to json
+				.then(fetchResponse => fetchResponse.json())
+
+				.then(data => {
+					// write json into idb reviews store
+					return idbPromise.then(idb => {
+						// new transaction
+						const tx = idb.transaction("reviews", "readwrite");
+						const store = tx.objectStore("reviews");
+						data.forEach(review => {
+							// place each in the store by id
+							store.put({
+								id: review.id,
+								"byrestaurantid": review["byrestaurantid"],
+								data: review
 							});
 						})
-					)
-				}
-
-			}).then(response => {
-				return new Response(JSON.stringify(response));
+						return data;
+					})
+				})
+		}).then(finalResponse => {
+			if (finalResponse[0].data) {
+				// convert data
+				const mapResponse = finalResponse.map(review => review.data);
+				// have to do a new response object
+				return new Response(JSON.stringify(mapResponse));
+			}
+			return new Response(JSON.stringify(finalResponse));
+		}).catch(error => {
+			return new Response("Oh Crud, there was an error", {
+				status: 500
 			})
-			
-			.catch(error => {
-				console.log('error in handleAPIRequest: ',error);
-				return new Response('Something went terribly wrong!', {status: 500 });
-			})
+		})
 	);
 }
