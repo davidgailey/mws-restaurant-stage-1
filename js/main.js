59f2a090d0bf33a75a61b3f95431d46224d7d573
David M. Gailey <dmg197@C02M63QDFD58.local>
2018-09-25 12:35:41 -0400

    feature: save review, break out service worker registration

--- js/main.js
+++ js/main.js
@@ -216,20 +216,5 @@ addMarkersToMap = (restaurants = self.restaurants) => {
   });
 } */
 
-// regiter service worker to cache static assets
-registerSW = () => {
-  if (!navigator.serviceWorker) return;
 
-  // using github pages for https - https://davidgailey.github.io/mws-restaurant-stage-1/sw.js
-  navigator.serviceWorker.register('/mws-restaurant-stage-1/sw.js').then( reg => {
-    
-    console.log('register serviceWorker success ', reg.scope);
-    
-  }).catch( err =>{
-    console.error('register serviceWorker failed', err);
-  });
-
-}
-
-registerSW();
 
